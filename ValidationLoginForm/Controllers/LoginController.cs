bc3902ed9a015cc109a7bc495b2f0ec25c30aede
Sarina Balami <157870784+sacrod-in-a-tech@users.noreply.github.com>
2026-01-01 14:51:01 +0545

    Add login validation to LoginController

--- ValidationLoginForm/Controllers/LoginController.cs
+++ ValidationLoginForm/Controllers/LoginController.cs
@@ -1,10 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ValidationLoginForm.Controllers
 {
     public class LoginController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                ViewBag.Message = "Please fill in all fields.";
+                return View();
+            }
+
+            return RedirectToAction("Success");
+        }
+
+        public IActionResult Success()
         {
             return View();
         }
